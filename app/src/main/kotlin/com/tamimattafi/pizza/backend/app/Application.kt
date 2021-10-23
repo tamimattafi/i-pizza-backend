6e30f0ceda9189fbd888b9817466346d6177a752
Tamim <attafitamim@gmail.com>
2021-10-24 02:57:59 +0700

    fix: Removed dependency

--- app/src/main/kotlin/com/tamimattafi/pizza/backend/app/Application.kt
+++ app/src/main/kotlin/com/tamimattafi/pizza/backend/app/Application.kt
@@ -1,9 +1,7 @@
 package com.tamimattafi.pizza.backend.app
 
-import com.tamimattafi.pizza.backend.presentation.controllers.PizzaController
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
